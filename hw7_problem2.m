1e4a748022bcc0e3b1baabcd7b2e4821a95508f1
Allen Nguyen <48102055+AllenNguyen1993@users.noreply.github.com>
2019-04-03 16:00:10 -0500

    Update hw7_problem2.m

--- hw7_problem2.m
+++ hw7_problem2.m
@@ -1,3 +1,11 @@
 k=input('please enter a value for k (1 is recommended): ');
+N_points=input ('Please enter number of internal points in space: ');
+t_points=input ('Please enter numbers of internal points in time: ');
+omega==input('Please enter a value for omega: ');
+T=10;
 L=pi;
 D=0.1;
+del_x
+del_x= L/(N_points+1);
+del_t= T/(t_points+1);
+
