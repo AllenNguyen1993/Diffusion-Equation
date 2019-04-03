5fe252ec126f87efb3d16318a20a2f21ce9b3e9b
Allen Nguyen <48102055+AllenNguyen1993@users.noreply.github.com>
2019-04-03 17:17:27 -0500

    Update hw7_problem2.m

--- hw7_problem2.m
+++ hw7_problem2.m
@@ -5,7 +5,17 @@ omega==input('Please enter a value for omega: ');
 T=10;
 L=pi;
 D=0.1;
-del_x
 del_x= L/(N_points+1);
 del_t= T/(t_points+1);
+gamma=(D*del_t)/(del_x)^2;
+F=zeros(N_points+2,t_points+2)
+g_0=zeros(1,t+2);
+g_L=zeros(1,t+2);
+
+for A= 1:t_points+2                %Computing the boundary conditions at different time points.
+  g_0(1)=sin(omega*del_t*(A-1));     %A=1 is index for t=0 and A=t_points+2 is index for the final time.
+  g_L(1)=sin(omega*del_t*(A-1));
+  t_j=del_t*(A-1);
+end
+
 
