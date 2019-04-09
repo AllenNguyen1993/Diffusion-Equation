133c1eab502636286f8467d73d5646df12b4dd2c
Allen Nguyen <allennguyen1993@gmail.com>
2019-04-09 13:43:22 -0500

    Signed-off-by: Allen Nguyen <allennguyen1993@gmail.com>

--- hw7_problem2.m
+++ hw7_problem2.m
@@ -1,16 +1,16 @@
 k=input('please enter a value for k (1 is recommended): ');
 N_points=input ('Please enter number of internal points in space: ');
 t_points=input ('Please enter numbers of internal points in time: ');
-omega=input('Please enter a value for omega: ');  %omega greater than 0.4 can make negative
+omega=input('Please enter a value for omega: ');  
 T=10;
 L=pi;
 D=0.1;
 del_x= L/(N_points+1);
 del_t= T/(t_points+1);
 gamma=(D*del_t)/(del_x)^2;
-F=zeros(N_points+2,t_points+2);
+F=zeros(N_points,t_points+1); 
 
-%Rows represent diffent positions;
+%Rows represent different positions;
 %Columns represent different times;
 t_j=[0,zeros(1,t_points),T];
 x_j=zeros(1,N_points);
@@ -20,12 +20,24 @@ for A=2:t_points+1    %Computing time point with given corresponding time indice
 end                     %1 corresponds to the first time point (t=0)
 
 for B=1:N_points
-    x_j(B)=del_x*(B)
+    x_j(B)=del_x*(B);
 end
 
 g_0=sin(omega*t_j);    %Computing boundary conditions at diffrerent time points
 g_L=sin(omega*t_j);
 u_numerical=[g_0;zeros(N_points,t_points+2);g_L];
+right_side_first=zero(1,t+1)    %first refers to the first element in the matrix on the right hand side
+right_side_last=zero(1,t+1)     %last refers to the last element in the matrix on the left had side
+right_side= [right_side_first;zero(N_points-2,t_points+1);right_side_last]; 
+a=zeros(1,N_points);
+b=zeros(1,N_points);
+c=zeros(1,N_points-1);
+
+for C=1:t_points+2
+    for E=
+        right_side(E,C)=
+        
+
 u_exact=(sin(omega*t_j(t_points+2)))*cos(k*x_j); 
 plot(x_j,u_exact)
  
