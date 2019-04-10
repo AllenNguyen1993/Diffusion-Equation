a2ef74a379606da7ac841b21baaaa281cf3c620b
Allen Nguyen <48102055+AllenNguyen1993@users.noreply.github.com>
2019-04-09 19:23:57 -0500

    Update hw7_problem2.m

--- hw7_problem2.m
+++ hw7_problem2.m
@@ -9,6 +9,12 @@ del_x= L/(N_points+1);
 del_t= T/(t_points+1);
 gamma=(D*del_t)/(del_x)^2;
 F=zeros(N_points,t_points+1); 
+a=(1+gamma)*ones(1,N_points);
+b=(-gamma/2)*ones(1,N_points);
+c=(-gamma/2)*ones(1,N_points-1);
+alpha_1=a(1);
+alpha=[alpha_1,zeros(1,N_points-1)];
+
 
 %Rows represent different positions;
 %Columns represent different times;
@@ -17,12 +23,17 @@ x_j=zeros(1,N_points);
 right_side_first=zeros(1,t_points+1)    %first refers to the first element in the matrix on the right hand side
 right_side_last=zeros(1,t_points+1)     %last refers to the last element in the matrix on the left had side
 
-for A=2:t_points+1    %Computing time point with given corresponding time indices
+for A=2:t_points+2    %Computing time point with given corresponding time indices
   t_j(A)=del_t*(A-1);  %t_points+2 is the last time point
 end                     %1 corresponds to the first time point (t=0)
 
 for B=1:N_points
     x_j(B)=del_x*(B);
+   
+end
+
+for AI=2:N_points
+     alpha(AI)=a(AI)-((b(AI)*c(AI-1))/alpha(AI-1));
 end
 
 g_0=sin(omega*t_j);    %Computing boundary conditions at diffrerent time points
@@ -31,9 +42,7 @@ u_numerical=[g_0;zeros(N_points,t_points+2);g_L];
 right_side_first=zeros(1,t_points+1)    %first refers to the first element in the matrix on the right hand side
 right_side_last=zeros(1,t_points+1)     %last refers to the last element in the matrix on the left had side
 right_side= [right_side_first;zeros(N_points-2,t_points+1);right_side_last]; 
-a=(1+gamma)*ones(1,N_points);
-b=(-gamma/2)*ones(1,N_points);
-c=(-gamma/2)*ones(1,N_points-1);
+
 
 for C=1:t_points+1
     
@@ -47,7 +56,15 @@ for C=1:t_points+1
     for G=2
       right_side(G,C)=(gamma/2)*u_numerical(G-1,C)+(1-gamma)*u_numerical(G,C)+(gamma/2)*u_numerical(G+1,C)+del_t*F(G,C);
     end
+    
+ g=[right_side(1,C);zeros(N_points-1,C)];
+ 
+    for H=2:N_points
+      g(H,C)=right_side(H,C)-((b(H)*g(H-1,C))/alpha(H-1);
+    end
+    
 end
+
 u_exact=(sin(omega*t_j(t_points+2)))*cos(k*x_j); 
 plot(x_j,u_exact)
  
